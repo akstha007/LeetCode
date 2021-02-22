d2c09d02646ed06cfd479e9227d39aa2d794373f
akstha007 <cs.ashok007@gmail.com>
2021-02-21 19:01:14 -0800

    443_String_Compression added

---
+++ 443_String_Compression.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;        
+        if(n<=1)
+            return chars.length;
+        
+        int res = 0;
+        int i=0,j=0;
+        while(j<n){
+            chars[res++] = chars[i];
+            while(j<n && chars[i]==chars[j]){
+                j++;
+            }
+            
+            if(j-i>1){
+                int diff = j -i;
+                for(char c: Integer.toString(j-i).toCharArray()){
+                    chars[res++] = c;
+                }
+            }
+            i = j;
+        }
+        
+        return res;
+    }
+}
